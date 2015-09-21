d79f6b98a6fce739a88b486cdcb47ae9fb563ae1
zoejf <zoejfoss@gmail.com>
2015-09-20 21:43:34 -0700

    trying to fix ENV issue

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,6 +77,8 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
+  config.secret_key_base = ENV["SECRET_KEY_BASE"]
+
   config.action_mailer.default_url_options = { host: 'https://scope-learning.herokuapp.com'}
   config.action_mailer.delivery_method = :smtp
 
