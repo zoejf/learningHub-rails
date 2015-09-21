55b841b8fdfae9d8d430a82f31faf0254df59ee6
zoejf <zoejfoss@gmail.com>
2015-09-20 21:15:02 -0700

    editing production.rb for heroku error

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,13 +79,4 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
 
-  config.action_mailer.smtp_settings = {
-    :address => "smtp.gmail.com",
-    :port => 587, 
-    :domain => 'scope-learning.herokuapp.com'
-    :authentication => :plain,
-    :user_name => ENV['gmail_username'],
-    :password => ENV['gmail_password'],
-    :enable_starttls_auto => true
-  }
 end
